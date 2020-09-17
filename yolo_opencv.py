d6fb412ac9a174c6e8fce21ee9ad51427689bf26
Kyle Morgenstein <34984693+KyleM73@users.noreply.github.com>
2020-09-17 01:15:17 -0400

    return image

--- yolo_opencv.py
+++ yolo_opencv.py
@@ -100,5 +100,6 @@ for i in indices:
 #cv2.imshow("object detection", image)
 #cv2.waitKey()
     
-#cv2.imwrite("object-detection.jpg", image)
+cv2.imwrite("object-detection.jpg", image)
 #cv2.destroyAllWindows()
+return image
